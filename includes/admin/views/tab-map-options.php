efebad3a02f29460f87d64c448ade371fc343dc3
Josh Pollock <jpollock412@gmail.com>
2016-02-25 00:53:08 -0500

    Add in common admin/views #6 #9
    Related commit: https://github.com/WordImpress/Maps-Builder-Pro/commit/a5df47e82bafbbf085775c9330a2a5a1d3140992

---
+++ includes/admin/views/tab-map-options.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Represents the tab view for Google Places widget.
+ * *
+ * @package   Google_Maps_Builder
+ * @author    Devin Walker <devin@wordimpress.com>
+ * @license   GPL-2.0+
+ * @link      http://wordimpress.com
+ * @copyright 2015 WordImpress, Devin Walker
+ */
+
+?>
+
+<div class="container">
+	<div class="row">
+
+		<div class="col-md-10">
+
+			<h3>
+				<?php _e('Default Map Settings', $plugin_slug); ?>
+			</h3>
+			<p>
+				<?php _e('The following settings change the default map options that display when created a new map.', $plugin_slug); ?>
+			</p>
+			<?php cmb2_metabox_form( $map_option_fields, $key ); ?>
+		</div>
+
+		<div class="col-md-2">
+
+		</div>
+	</div>
+</div>
---
+++ includes/admin/views/tab-system-info.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * System Info Tab - Displays useful debugging info for support reasons
+ *
+ * @package   Google_Maps_Builder
+ * @author    Devin Walker <devin@wordimpress.com>
+ * @license   GPL-2.0+
+ * @link      http://wordimpress.com
+ * @copyright 2015 WordImpress, Devin Walker
+ */
+?>
+
+<div class="container">
+	<div class="row">
+		<div class="col-md-10">
+
+			<h3><?php _e( 'System Info', $plugin_slug ); ?></h3>
+
+			<p><?php _e( 'The following displays useful information about your website that may be requested from you for support reasons.', $plugin_slug ); ?></p>
+
+			<form class="gmb-form" method="post" id="system_settings" enctype="multipart/form-data" encoding="multipart/form-data">
+				<?php gmb_system_info_callback(); ?>
+			</form>
+
+		</div>
+		<div class="col-md-2">
+
+		</div>
+	</div>
+</div>
