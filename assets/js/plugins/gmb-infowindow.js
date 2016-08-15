fdaf26eb703a5aea950bf40c5fc4fc5e21406d75
Lucas Cherkewski <lucas@ecustom.ca>
2016-08-15 17:41:16 -0400

    separate code out to use the InfoBubble method of organisation

--- assets/js/plugins/gmb-infowindow.js
+++ assets/js/plugins/gmb-infowindow.js
@@ -10,75 +10,88 @@
  *     - https://developers.google.com/maps/documentation/javascript/customoverlays
  */
 
-google.maps.GMB_InfoWindow = (function() {
-   var InfoWindow = function() {
-       this.container = jQuery('<div class="gmb-infobuble"></div>');
-       this.layer = null;
-       this.marker = null;
-       this.position = null;
-   };
+function GMB_InfoWindow() {
+    // Inherit from OverlayView
+    this.extend(GMB_InfoWindow, google.maps.OverlayView);
 
-    /**
-     * Inherit from OverlayView
-     */
-    InfoWindow.prototype = new google.maps.OverlayView();
+    this.container = jQuery('<div class="gmb-infobuble"></div>');
+    this.layer = null;
+    this.marker = null;
+    this.position = null;
+}
 
-    /**
-     * Called when window is added to map
-     */
-    InfoWindow.prototype.onAdd = function() {
-        this.layer = jQuery(this.getPanes().floatPlane);
-        this.layer.append(this.container);
-        this.container.find('.map-info-close').on('click', _.bind(function() {
-            // Close info window on click
-            this.close();
-        }, this));
-    };
+// Attach our function to the window
+window['GMB_InfoWindow'] = GMB_InfoWindow;
 
-    /**
-     * Redraws the window any time something happens to affect its position
-     */
-    InfoWindow.prototype.draw = function() {
-        var markerIcon = this.marker.getIcon(),
-            cHeight = this.container.outerHeight() + markerIcon.scaledSize.height + 10,
-            cWidth = this.container.width() / 2 + markerIcon.scaledSize.width / 2;
+// Extension method from InfoBubble
+InfoBubble.prototype.extend = function(obj1, obj2) {
+    return (function(object) {
+        for (var property in object.prototype) {
+            this.prototype[property] = object.prototype[property];
+        }
+        return this;
+    }).apply(obj1, [obj2]);
+};
 
-        this.position = this.getProjection().fromLatLngToDivPixel(this.marker.getPosition());
+/**
+ * Called when window is added to map
+ */
+GMB_InfoWindow.prototype.onAdd = function() {
+    this.layer = jQuery(this.getPanes().floatPlane);
+    this.layer.append(this.container);
+    this.container.find('.map-info-close').on('click', _.bind(function() {
+        // Close info window on click
+        this.close();
+    }, this));
+};
+GMB_InfoWindow.prototype['onAdd'] = GMB_InfoWindow.prototype.onAdd;
 
-        this.container.css({
-            'top': this.position.y - cHeight,
-            'left': this.position.x - cWidth
-        });
-    };
+/**
+ * Redraws the window any time something happens to affect its position
+ */
+GMB_InfoWindow.prototype.draw = function() {
+    var markerIcon = this.marker.getIcon(),
+        cHeight = this.container.outerHeight() + markerIcon.scaledSize.height + 10,
+        cWidth = this.container.width() / 2 + markerIcon.scaledSize.width / 2;
 
-    /**
-     * Cleanup function when the window is removed from map
-     */
-    InfoWindow.prototype.onRemove = function() {
-        this.container.remove();
-    };
+    this.position = this.getProjection().fromLatLngToDivPixel(this.marker.getPosition());
 
-    /**
-     * Set the contents of the overlay container
-     */
-    InfoWindow.prototype.setContent = function( html ) {
-        this.container.html(html);
-    };
+    this.container.css({
+        'top': this.position.y - cHeight,
+        'left': this.position.x - cWidth
+    });
+};
+GMB_InfoWindow.prototype['draw'] = GMB_InfoWindow.prototype.draw;
 
-    /**
-     * Add the window to a specific map marker (thus displaying it)
-     */
-    InfoWindow.prototype.open = function( map, marker ) {
-        this.marker = marker;
-        this.setMap(map);
-    };
+/**
+ * Cleanup function when the window is removed from map
+ */
+GMB_InfoWindow.prototype.onRemove = function() {
+    this.container.remove();
+};
+GMB_InfoWindow.prototype['onRemove'] = GMB_InfoWindow.prototype.onRemove;
 
-    /**
-     * Remove the window from any specific map (thus hiding it)
-     */
-    InfoWindow.prototype.close = function() {
-        this.setMap(null);
-    };
+/**
+ * Set the contents of the overlay container
+ */
+GMB_InfoWindow.prototype.setContent = function( html ) {
+    this.container.html(html);
+};
+GMB_InfoWindow.prototype['setContent'] = GMB_InfoWindow.prototype.setContent;
 
-    return InfoWindow;
-});
+/**
+ * Add the window to a specific map marker (thus displaying it)
+ */
+GMB_InfoWindow.prototype.open = function( map, marker ) {
+    this.marker = marker;
+    this.setMap(map);
+};
+GMB_InfoWindow.prototype['open'] = GMB_InfoWindow.prototype.open;
+
+/**
+ * Remove the window from any specific map (thus hiding it)
+ */
+GMB_InfoWindow.prototype.close = function() {
+    this.setMap(null);
+};
+GMB_InfoWindow.prototype['close'] = GMB_InfoWindow.prototype.close;
