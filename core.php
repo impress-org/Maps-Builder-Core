7b86909de2603cd73ff7a1d07277158d6e2cc6fe
Devin Walker <devin@wordimpress.com>
2016-09-29 11:16:34 -0700

    Removed false @since PHP doc

--- core.php
+++ core.php
@@ -5,7 +5,6 @@
  * @package     GMB-Core
  * @copyright   Copyright (c) 2016 WordImpress
  * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
- * @since       0.1.0
  */
 
 // Exit if accessed directly
